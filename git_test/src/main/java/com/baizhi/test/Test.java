40d5db2c772e239bb128c0e19997a32631e5b495
Your Name <you@example.com>
2018-10-19 17:02:07 +0800

    the 2nd version

--- git_test/src/main/java/com/baizhi/test/Test.java
+++ git_test/src/main/java/com/baizhi/test/Test.java
@@ -3,5 +3,9 @@ package com.baizhi.test;
 public class Test {
     public static void main(String[] args) {
 
+        System.out.println("我是帅比");
+        System.out.println("我是帅比");
+        System.out.println("我是帅比");
+        System.out.println("我是帅比");
     }
 }
